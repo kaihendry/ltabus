52309e4740ceea320fdb556233f72b4d1ce4a99d
Kai Hendry <hendry@webconverger.com>
2018-12-29 17:51:54 +0800

    Retrieve bus information to all.json

---
+++ allthebusstops.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+test -f .env && source .env
+
+if ! test "$accountkey"
+then
+	echo env AccountKey is unset
+	exit
+fi
+
+pwd=$PWD
+cd "$(mktemp -d)" || exit
+
+count=0
+while :
+do
+curl -s -f -X GET https://api.mytransport.sg/ltaodataservice/BusStops/?\$skip=$count -H "AccountKey: $accountkey" |
+	jq .value[] > $count.json
+	test -s "$count.json" || break
+	count=$((count+500))
+done
+
+jq . ./*.json | jq -s . > "$pwd/all.json"
+echo "Bus stop count $(jq 'length' "$pwd/all.json")"
